712d971ffc051cd7feae4d2e499b4278adb0e7da
Athanasios Nektarios Karachalios Stagkas <nasos.karachalios.stagas@gmail.com>
2022-03-07 00:08:28 +0200

    fixedScrollingScript
    scorlling Script finished
    needed a workaround around oposite vectors so i used magic numbers instead to save time

---
+++ Thesis/Assets/TextContentSizeFinder.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextContentSizeFinder : MonoBehaviour
+{
+    private Vector2 currentVerticalSize, initialOffsetMax;
+    private RectTransform rect;
+    private TextMeshProUGUI tmpproCurrent;
+    private ScrollRect scroll;
+
+    private void Awake()
+    {
+        rect = GetComponent<RectTransform>();
+        scroll=GetComponentInParent<ScrollRect>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialOffsetMax = rect.offsetMax;
+        Orders();
+
+    }
+    private void OnEnable()
+    {
+        Orders();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FindTextSize();
+    }
+    private void Orders()
+    {
+
+        GetCurrentEnabledChild();
+        FindTextSize(); ;
+        SetBottomSpace();
+
+    }
+    void FindTextSize()
+    {
+        tmpproCurrent.ForceMeshUpdate();
+        currentVerticalSize = tmpproCurrent.GetRenderedValues();
+    }
+    private void GetCurrentEnabledChild()
+    {
+        tmpproCurrent = GetComponentInChildren<TextMeshProUGUI>();
+
+
+    }
+    private void SetBottomSpace()
+    {
+        Vector2 previousMaxVector=rect.offsetMax;
+        rect.offsetMin = new Vector2(rect.offsetMin.x,-currentVerticalSize.y+0.1f*currentVerticalSize.y);
+        rect.offsetMax= new Vector2(rect.offsetMax.x,1);
+        rect.ForceUpdateRectTransforms();
+    }
+}
